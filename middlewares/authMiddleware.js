013743eaeee43fe4fb55c08058afc688f0337d01
AtulShinde9552 <113991404+AtulShinde9552@users.noreply.github.com>
2024-06-12 03:34:10 +0530

    NA

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports.authMiddleware = async (req, res, next) => {
     const {authorization} = req.headers
     if (authorization) {
-        const token = authorization.split('Bearer')[1]
+        const token = authorization.split('Bearer ')[1]
     if (!token) {
         return res.status(409).json({ error: 'Please login first' })
     } else {
